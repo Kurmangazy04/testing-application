5f3024df57e0f8b213208960172392e2e195e3b5
Kurmangazy Orynbasarov <kurmangazyorynbasarov@MacBook-Air-Kurmangazy.local>
2024-02-08 22:57:45 +0600

    first commit

---
+++ src/pages/NoPage.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+export default function Nopage(){
+    return(
+        <>
+        <Header/>
+        <h2>Error 404: Not found</h2>
+        </>
+    )
+}
